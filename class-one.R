a65486036bac18ceb2f2d0070c15b475d8ce968f
Leo Bruell <leobruell@gmail.com>
2022-09-09 09:28:18 -0400

    add project

---
+++ class-one.R
@@ -0,0 +1,17 @@
+#First Class Notes 
+4/3 #This returns float because R is designed for stats.
+4 * 2 + 3 #R follows PEMDAS
+
+x <- 2 #Variable assignment
+x
+y = 3 #alternative assignment
+
+rm(y)
+y
+x + 1
+
+2 > 3
+2 >= 3
+2 == 3
+2 < 3
+2 != 3
