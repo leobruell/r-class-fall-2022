666c53fd0ab62bc0e18c392f3baf957d33ccb926
Leo Bruell <leobruell@gmail.com>
2022-09-09 09:33:42 -0400

    Work until first break

--- class-one.R
+++ class-one.R
@@ -2,9 +2,9 @@
 4/3 #This returns float because R is designed for stats.
 4 * 2 + 3 #R follows PEMDAS
 
-x <- 2 #Variable assignment
+x <- 2 #Variable assignment ####
 x
-y = 3 #alternative assignment
+y = 3 #alternative assignment 
 
 rm(y)
 y
@@ -15,3 +15,25 @@ x + 1
 2 == 3
 2 < 3
 2 != 3
+
+# Vectors ####
+x <- c(1, 3, 8)
+x + 1
+x
+z <- x+1
+z
+x <- 7
+z
+x <- c(4, 2, 10)
+x*2
+x^3
+y <- c(1,2,3)
+x + y
+x < y
+# If vectors are different lengths, R will start to loop through 
+# the short one.
+
+a_hundred = 1:100
+a_hundred[4]
+sum(a_hundred)
+mean(a_hundred)
